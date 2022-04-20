5e7e68252284d1ef7c231b3e081ed0077e10fd83
Fevi Roma <foevaroma@gmail.com>
2022-04-20 14:55:41 +0200

    handlebars

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 app.use(express.static('public')); // must be placed allways after const app = express();
 
@@ -7,21 +10,68 @@ app.use(express.static('public')); // must be placed allways after const app = e
 //app.get(path, code);
 //path for homepage "/"
 
+
+/* Routes */
+
+app.get("/", (req, res, next) => {
+    res.render("home");
+});
+
+
+
 app.get("/about", (req, res, next) => {
-    //   console.log(req);
-    //  console.log("a request on the ABOUT page was received");
-    // res.send("this is your about page");
-
-
-    res.sendFile(__dirname + '/views/about.html');
+    res.render("about");
 });
 
 
 app.get("/contact", (req, res, next) => {
 
 
-    res.sendFile(__dirname + '/views/contact.html');
+    res.render("contact");
 });
+
+app.get("/limoncello", (req, res, next) => {
+
+    // res.render("view", info);
+
+    const data = {
+        title: "Limoncello",
+        price: 10,
+        imageFile: "mouri 1 y.jpg",
+        stores: ["Online", "Albacete", "Freiburg", "Amsterdam"]
+    }
+
+    res.render("product", data);
+});
+
+app.get("/whisky", (req, res, next) => {
+
+    // res.render("view", info);
+
+    const data = {
+        title: "Whisky",
+        price: 80,
+        imageFile: "mourimou.jpg",
+        stores: ["Online", "Berlin", "Paris", "Rome"]
+    }
+
+    res.render("product", data);
+});
+
+app.get("/tequila", (req, res, next) => {
+
+    // res.render("view", info);
+
+    const data = {
+        title: "Tequila",
+        price: 20,
+        imageFile: "mourisofa.jpg",
+        stores: ["Online", "Athens", "Santorini", "Porto"]
+    }
+
+    res.render("product", data);
+});
+
 
 
 
